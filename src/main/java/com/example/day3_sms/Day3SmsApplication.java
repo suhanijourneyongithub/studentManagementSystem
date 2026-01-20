235f925e4e197238e861c77dbb9ee4a50760aecd
Suhani Sharma <145221387+suhanijourneyongithub@users.noreply.github.com>
2026-01-20 11:50:14 +0530

    built mvc architecture

---
+++ src/main/java/com/example/day3_sms/Day3SmsApplication.java
@@ -0,0 +1,13 @@
+package com.example.day3_sms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day3SmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Day3SmsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/day3_sms/Day3SmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.day3_sms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day3SmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
